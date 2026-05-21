cbfa7771db8eb4ac74bfdd9cbe6849c89b197b98
Marios  Kanellopoulos <mkanellopoulos@cognity.gr>
2026-05-21 13:26:20 +0300

    Initial Commit

---
+++ Finance Dark Theme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance Dark Theme
+//
+//  Created by mkanellopoulos on 21/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance Dark Theme/Finance_Dark_ThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_Dark_ThemeApp.swift
+//  Finance Dark Theme
+//
+//  Created by mkanellopoulos on 21/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Finance_Dark_ThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
