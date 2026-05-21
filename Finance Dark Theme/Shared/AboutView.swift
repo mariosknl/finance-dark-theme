728161db37ea0f67c6b0346c4e3c10296b639258
Marios  Kanellopoulos <mkanellopoulos@cognity.gr>
2026-05-21 13:51:31 +0300

    Color Palette and the Avatar View

--- Finance Dark Theme/Shared/AboutView.swift
+++ Finance Dark Theme/Shared/AboutView.swift
@@ -12,18 +12,54 @@ struct AboutView: View {
         ZStack {
             darkBackground
                     .ignoresSafeArea()
-            VStack {
-                HStack {
-                    Spacer()
-                    Text("x")
-                        .font(.title)
-                        .padding()
-                        .foregroundColor(fontColor)
-                        .background(darkBackground2)
-                        .cornerRadius(10)
-                }
+            VStack(alignment: .leading) {
+         
+                xView()
+                
+                AvatarView()
+                
                 Spacer()
             }
+        }
+    }
+}
+
+struct TextView: View {
+    var text: String
+    var font: Font
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(fontColor)
+            .font(font)
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(systemName: "person.crop.circle")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundColor(fontColor)
+                .background(darkBackground2)
+                .clipShape(Circle())
+            TextView(text: "Marios Knl", font: .largeTitle)
+            TextView(text: "iOS Developer", font: .headline)
+        }
+    }
+}
+
+struct xView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("x")
+                .font(.title)
+                .padding()
+                .foregroundColor(fontColor)
+                .background(darkBackground2)
+                .cornerRadius(10)
         }
     }
 }
